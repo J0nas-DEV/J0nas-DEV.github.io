a19b32bf9034395706efd0d90d4128a245be0543
J0nas-DEV <99317564+J0nas-DEV@users.noreply.github.com>
2026-06-27 00:17:08 +0800

    1
    1

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-setInterval(()=>{
-    const now = new Date();
-    const date = now.toLocaleDateString();
-    const time = now.toLocaleTimeString();
-    document.getElementById('time').textContent = `${time} ${date}`;
-}, 1000)
